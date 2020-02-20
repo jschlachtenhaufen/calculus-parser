6fa21f9ae3b423686dd86b91fd055c18858489f5
John Schlachtenhaufen <jschlach25@gmail.com>
2020-02-20 11:35:50 -0500

    parsing var and func

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -4,10 +4,13 @@ module Parsing where
 import Expressions
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import Text.Megaparsec.Char.Lexer
+import Text.Megaparsec.Char.Lexer (decimal)
 import Data.Void
 
 type Parser = Parsec Void String
+
+-- sampleInput = "deriv(x, x * y)"
+-- sampleInput2 = "sin(5 + x) / (deriv(x, x^2))"
 
 expr :: Parser Expr
 expr =
@@ -16,11 +19,20 @@ expr =
     <|> TermFunc <$> func <*> many expr
     <|> TermOp <$> operator <*> expr <*> expr
 
+-- x, y2
 var :: Parser String
-var = "x1"
+var = try $
+    do c <- letterChar
+       rest <- many digitChar
+       return (c:rest) 
 
+-- sin, lambda, 
 func :: Parser String
-func = "sin"
+func = try $ 
+    do c1 <- letterChar
+       c2 <- letterChar
+       rest <- many alphaNumChar
+       return (c1:c2:rest)
 
 operator :: Parser String
-operator = "+" <|> "*"
+operator = "+" <|> "*" <|> "/" <|> "-"
