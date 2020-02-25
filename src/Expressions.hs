318543c97758444b0eedda84701a569da698cd13
John Schlachtenhaufen <jschlach25@gmail.com>
2020-02-25 15:15:16 -0500

    refactor code

--- src/Expressions.hs
+++ src/Expressions.hs
@@ -1,4 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Expressions where
+
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer (decimal)
+import Data.Void
+
 
 sampleInput1, sampleInput2 :: String
 sampleInput1 = "deriv(x, x * y)"
@@ -9,3 +16,62 @@ data Expr =
     | ConstN Double -- 3,5,2
     | TermFunc String [Expr] -- sin, deriv w/ args, lambda
     | TermOp String Expr Expr deriving (Eq, Show) -- -,+,*,/ with args
+
+type Parser = Parsec Void String
+
+expr :: Parser Expr
+expr = ((factor >>= third) >>= second) >>= first
+
+factor :: Parser Expr
+factor = ("(" *> expr <* ")") <|> termFunc <|> var <|> constN
+
+-- first, second, third in order of operation precedence
+first :: Expr -> Parser Expr
+first e1 = do {op <- space *> addop <* space;
+               e2 <- (factor >>= second);
+               first (TermOp op e1 e2)} <|> return e1
+
+second :: Expr -> Parser Expr
+second e1 = do {op <- mulop;
+                e2 <- (factor >>= third);
+                second (TermOp op e1 e2)} <|> return e1
+
+third :: Expr -> Parser Expr
+third e1 = do {op <- powop;
+               e2 <- factor;
+               third (TermOp op e1 e2)} <|> return e1
+
+-- x, y2
+var :: Parser Expr
+var = -- try $
+    do c <- letterChar
+       rest <- many digitChar
+       return (Var (c:rest))
+
+-- 5, 78
+constN :: Parser Expr
+constN = ConstN <$> decimal
+
+-- sin(x, 5*x^2)
+termFunc :: Parser Expr
+termFunc = TermFunc <$> func <*> (("(" *> exprArgs <* ")") <|> exprArgs)
+
+exprArgs :: Parser [Expr]
+exprArgs = try $ sepBy expr (char ',' <* space)
+
+-- sin, lambda, 
+func :: Parser String
+func = try $ 
+    do c1 <- letterChar
+       c2 <- letterChar
+       rest <- many alphaNumChar
+       return (c1:c2:rest)
+
+addop :: Parser String
+addop = "+" <|> "-"
+    
+mulop :: Parser String
+mulop =  "*" <|> "/"
+
+powop :: Parser String
+powop =  "^"
